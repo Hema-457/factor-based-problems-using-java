b29f4a069e0e5ced5285770a12c6f77b5a6da61c
Hema-457 <tamaranahema10@gmail.com>
2026-04-29 00:57:14 +0530

    Create Perfect, Abundant, Deficient Number.java

---
+++ Perfect, Abundant, Deficient Number.java
@@ -0,0 +1,18 @@
+class Main{
+public static void main (String[] args) 
+    {
+        int n=20;
+        int sum=0;
+        for(int i=1;i<=n/2;i++)
+        {
+            if(n % i == 0)
+            sum+=i;
+        }
+        if(sum==n)
+        System.out.println("Perfect Number");
+        else if(sum>n)
+        System.out.println("Abundant Number");
+        else
+        System.out.println("Deficient Number");
+    }
+}
